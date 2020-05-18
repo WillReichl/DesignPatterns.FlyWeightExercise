307f632f4fd93451e9435eb08c1a3288f8cb440b
Will Reichl <William.Reichl@dcsg.com>
2020-05-17 22:21:52 -0400

    Solution

--- FlyWeightCode/Sentence.cs
+++ FlyWeightCode/Sentence.cs
@@ -1,26 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Coding.Exercise
 {
     // https://www.udemy.com/course/design-patterns-csharp-dotnet/learn/quiz/350374#overview
     public class Sentence
     {
-        private string text;
+        private string[] words;
+        private Dictionary<int, WordToken> tokens = new Dictionary<int, WordToken>();
+
         public Sentence(string plainText)
         {
+            words = plainText.Split(' ');
         }
 
         public WordToken this[int index]
         {
             get
             {
-                // todo
+                WordToken wt = new WordToken();
+                tokens.Add(index, wt);
+                return tokens[index];
             }
         }
 
         public override string ToString()
         {
-            // output formatted text here
+            var ws = new List<string>();
+            for (var i = 0; i < words.Length; i++)
+            {
+                var w = words[i];
+                if (tokens.ContainsKey(i) && tokens[i].Capitalize)
+                    w = w.ToUpperInvariant();
+                ws.Add(w);
+            }
+            return string.Join(" ", ws);
         }
 
         public class WordToken
